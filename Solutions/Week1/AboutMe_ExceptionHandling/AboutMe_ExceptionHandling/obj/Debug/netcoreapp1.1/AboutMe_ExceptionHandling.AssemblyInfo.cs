6ffeeabc548d1b61f1b75808e2531a6f0a842474
Iverson, Amanda <amanda@codefellows.com>
2017-12-15 19:00:21 -0800

    updating lab 2

--- Solutions/Week1/AboutMe_ExceptionHandling/AboutMe_ExceptionHandling/obj/Debug/netcoreapp1.1/AboutMe_ExceptionHandling.AssemblyInfo.cs
+++ Solutions/Week1/AboutMe_ExceptionHandling/AboutMe_ExceptionHandling/obj/Debug/netcoreapp1.1/AboutMe_ExceptionHandling.AssemblyInfo.cs
@@ -13,7 +13,6 @@ using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("AboutMe_ExceptionHandling")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
-[assembly: System.Reflection.AssemblyDescriptionAttribute("Package Description")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("AboutMe_ExceptionHandling")]
