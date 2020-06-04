45ebc31fbb5f40a54858881c22e68385829a7354
amanda <ajohnson764@gmail.com>
2020-06-04 12:35:55 -0700

    Class 27 Demo Code

---
+++ Curriculum/Extra_Lectures/JWTAuth/Class27/Demo/IdentityDemo/IdentityDemo/Models/Services/PostService.cs
@@ -0,0 +1,55 @@
+﻿using IdentityDemo.Data;
+using IdentityDemo.Models.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityDemo.Models.Services
+{
+    public class PostService : IPostManager
+    {
+        private BlogDbContext _context;
+
+        public PostService(BlogDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task CreatePost(Post post)
+        {
+            _context.Entry(post).State = EntityState.Added;
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeletePost(int id)
+        {
+            var post = await _context.Posts.FindAsync(id);
+            _context.Entry(post).State = EntityState.Deleted;
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<List<Post>> GetAllPosts()
+        {
+            return await _context.Posts.ToListAsync();
+        }
+
+        public async Task<Post> GetPost(int id)
+        {
+            var post = await _context.Posts.FindAsync(id);
+            return post;
+        }
+
+        public async Task<Post> UpdatePost(Post post, int id)
+        {
+            if (post.Id == id)
+            {
+                _context.Entry(post).State = EntityState.Modified;
+                await _context.SaveChangesAsync();
+            }
+
+            return post;
+        }
+    }
+}
