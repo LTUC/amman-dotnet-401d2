66d3f8547cd6c4167b356cb077c7b989992442b5
Anas Atef Alrantisi <a.alrantisi@ltuc.com>
2023-06-20 15:30:48 +0300

    Class-04 Demo

---
+++ class-04/demo/BasicsOfClasses/BasicsOfClasses/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace BasicsOfClasses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string userName1 = "Ahmad";
+            int userAge1 = 30;
+            string MaritalStatus1 = "Single";
+            
+
+            string userName2 = "Anas";
+            int userAge2 = 30;
+            string MaritalStatus2 = "Single";
+
+
+            Console.WriteLine("Hello, World!");
+        }
+    }
+
+    public class User
+    {
+        public string Name;
+        public int Age;
+        public string MateriaStatus;
+
+    }
+}
