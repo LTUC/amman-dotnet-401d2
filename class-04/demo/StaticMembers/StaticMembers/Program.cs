5fb87ccc3bb01eee5cd8b54330dd78da5d327469
Anas Atef Alrantisi <a.alrantisi@ltuc.com>
2023-06-22 13:37:50 +0300

    Class-05 Demo

---
+++ class-04/demo/StaticMembers/StaticMembers/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace StaticMembers
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle circle1 = new Circle(5);
+            Circle circle2 = new Circle(10);
+
+            Console.WriteLine($"Numbers of circiles = {Circle.Instances}");
+
+            var areateOfTheFirstCircle = Circle.CalculateArea(circle1.Radius);
+
+            Console.WriteLine($"The Area of the first cicle is = {areateOfTheFirstCircle}");
+        }
+    }
+
+    public class Circle
+    {
+        public static float Pi = 3.14159f;
+        public static int Instances = 0;
+
+        public float Radius { get; set; }
+
+
+        public Circle(float radius)
+        {
+            this.Radius = radius;
+            Instances++;
+        }
+
+
+
+        public static float CalculateArea(float radius)
+        {
+            return Pi * radius * radius;
+        }
+    }
+
+}
---
+++ class-05/demo/LinkedListImplemetation/LinkedListImplemetation/Program.cs
@@ -0,0 +1,120 @@
+﻿namespace LinkedListImplemetation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList myList = new LinkedList();
+
+            // Adding elements
+            myList.AddToTheLast(10);
+
+            myList.AddToTheLast(20);
+
+            myList.AddToTheLast(30);
+
+            Console.WriteLine("Elements in the list");
+
+           // Console.WriteLine(myList.head.Data);
+
+            Node currentNode = myList.head;
+
+            while (currentNode != null)
+            {
+                Console.WriteLine(currentNode.Data);
+                currentNode = currentNode.Next;
+            }
+
+            // Insert a new element at position 1
+            myList.InsertAt(15, 1);
+
+            Console.WriteLine("Elments in the linked list after insetion");
+
+            Node currentNode2 = myList.head;
+
+            while (currentNode2 != null)
+            {
+                Console.WriteLine(currentNode2.Data);
+                currentNode2 = currentNode2.Next;
+            }
+
+        }
+    }
+
+    public class Node
+    {
+        public int Data { get; set; }
+
+        public Node Next { get; set; }
+
+        public Node(int data)
+        {
+            Data = data;
+            Next = null;
+        }
+    }
+
+    public class LinkedList
+    {
+        public Node head { get; set; }
+        public Node tail { get; set; }
+
+        public LinkedList()
+        {
+            head = null;
+            tail = null;
+        }
+
+        public void AddToTheLast(int data)
+        {
+            Node newNode = new Node(data);
+
+            if(head == null)
+            {
+                head = newNode;
+                tail = newNode;
+            }
+            else
+            {
+                
+                tail.Next = newNode;
+
+                tail = newNode;
+            }
+        }
+
+
+        // AddFirst, InsertAt, Remove
+
+        public void InsertAt(int data, int position)
+        {
+            Node newNode = new Node(data);
+            Node previousNode = null;
+            Node currentNode = head;
+            int currentPostion = 0;
+
+            while(currentNode != null && currentPostion < position) 
+            {
+                previousNode = currentNode;
+                currentNode = currentNode.Next; 
+                currentPostion++;
+
+            }
+
+            if(currentPostion == position)
+            {
+                previousNode.Next = newNode;
+                newNode.Next = currentNode;
+
+                if (currentNode == null)
+                {
+                    tail = newNode;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Posion out of range.");
+            }
+        }
+    }
+}
