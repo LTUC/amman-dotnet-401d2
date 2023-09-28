f10385206c32fa7bcebac138d18b1ef354ee7e15
Anas Atef Alrantisi <a.alrantisi@ltuc.com>
2023-09-28 12:14:13 +0300

    Class-35 Demo

---
+++ class-35/demo/GraphDemo/GraphDemo/Graph.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraphDemo
+{
+	public class Vertex<T>
+	{
+        public T Value { get; set; }
+
+        public Vertex(T value)
+        {
+			Value = value;
+		}
+    }
+
+	public class Edge<T>
+	{
+        public int Weight { get; set; }
+        public Vertex<T> Vertex { get; set; }
+    }
+	public class Graph<T>
+	{
+        public Dictionary<Vertex<T>, List<Edge<T>>> AdjacenceyList { get; set; }
+
+        private int _size = 0;
+
+        public Graph()
+        {
+            AdjacenceyList = new Dictionary<Vertex<T>, List<Edge<T>>>();
+		}
+
+        public Vertex<T> AddVertex(T vertex)
+        {
+            Vertex<T> node = new Vertex<T>(vertex);
+
+			AdjacenceyList.Add(node, new List<Edge<T>>());
+
+            _size++;
+
+			return node;
+                
+        }
+
+        public void AddDirectEdge(Vertex<T> a, Vertex<T> b)
+        {
+            AdjacenceyList[a].Add(new Edge<T>
+            {
+                Weight = 0,
+                Vertex = b,
+            });
+
+		}
+
+        public void AddUnDirectEdge(Vertex<T> a, Vertex<T> b)
+        {
+            AddDirectEdge(a, b);
+			AddDirectEdge(b, a);
+		}
+
+        public List<Edge<T>> GetNeighbors(Vertex<T> vertex) 
+        {
+            return AdjacenceyList[vertex];
+        }
+
+        public void Print()
+        {
+            foreach (var item in AdjacenceyList)
+            {
+                Console.Write($"Vertex {item.Key.Value} =>");
+
+                foreach (var edge in item.Value)
+                {
+                    Console.Write($"{edge.Vertex.Value} =>");
+                }
+
+                Console.WriteLine();
+            }
+        }
+	}
+}
---
+++ class-35/demo/GraphDemo/GraphDemo/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace GraphDemo
+{
+	public class Program
+	{
+		static void Main(string[] args)
+		{
+			Graph<string> graph = new Graph<string>();
+
+			Vertex<string> a = graph.AddVertex("Amman");
+			Vertex<string> b = graph.AddVertex("Irbid");
+			Vertex<string> c = graph.AddVertex("Salt");
+
+			// Complete graph
+			graph.AddUnDirectEdge(a, b);
+			graph.AddUnDirectEdge(b, c);
+			graph.AddUnDirectEdge(c, a);
+
+			graph.Print();
+		}
+	}
+}
